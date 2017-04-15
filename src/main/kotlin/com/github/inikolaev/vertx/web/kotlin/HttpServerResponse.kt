c527dfb3793da3046d8686ef5a7453e440a67a74
Igor Nikolaev <igor.nikolaev@zalando.fi>
2017-04-15 19:22:40 +0300

    Support JSON arrays in endJson extension function

--- src/main/kotlin/com/github/inikolaev/vertx/web/kotlin/HttpServerResponse.kt
+++ src/main/kotlin/com/github/inikolaev/vertx/web/kotlin/HttpServerResponse.kt
@@ -1,6 +1,8 @@
 package com.github.inikolaev.vertx.web.kotlin
 
 import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 
 fun HttpServerResponse.endJson(json: JsonObject) = this.end(json.encode())
+fun HttpServerResponse.endJson(json: JsonArray) = this.end(json.encode())
