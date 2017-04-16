cfb29d84aef05ad40464866287bf1143e4f1e589
Igor Nikolaev <igor.nikolaev@zalando.fi>
2017-04-16 16:30:17 +0300

    Make endJson to encode any object to JSON

--- src/main/kotlin/com/github/inikolaev/vertx/web/kotlin/HttpServerResponse.kt
+++ src/main/kotlin/com/github/inikolaev/vertx/web/kotlin/HttpServerResponse.kt
@@ -1,8 +1,10 @@
 package com.github.inikolaev.vertx.web.kotlin
 
 import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.json.Json
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 
 fun HttpServerResponse.endJson(json: JsonObject) = this.end(json.encode())
 fun HttpServerResponse.endJson(json: JsonArray) = this.end(json.encode())
+fun HttpServerResponse.endJson(entity: Any) = this.end(Json.encode(entity))
